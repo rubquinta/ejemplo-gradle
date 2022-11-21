9d31d5d13b5eb5db47d24f5604b13adf80c77f05
rubquinta <rubemo05@gmail.com>
2022-11-20 22:57:49 -0300

    JenkinsFile con params

--- gradle.groovy
+++ gradle.groovy
@@ -1,19 +1,19 @@
 def gradle_build(){
-    stage{
+    
         sh 'gradle build'
-    }
+    
 }
 
 def gradle_run(){
-    stage{
+    
         sh 'gradle bootRun'
-    }
+    
 }
 
 def gradle_test(){
-    stage{
+    
         sh 'Test: curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
-    }
+    
 }
 
 return this;
