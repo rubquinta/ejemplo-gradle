697f2a68a744e89181d6d4e2491358755985004d
rubquinta <rubemo05@gmail.com>
2022-11-21 22:27:50 -0300

    gradle con sleep 30

--- gradle.groovy
+++ gradle.groovy
@@ -6,6 +6,7 @@ def gradle_build_test_jar()
 def gradle_run()
         {
                 sh "gradle bootRun &"
+                sh "sleep 30"
                 sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
         }
 
